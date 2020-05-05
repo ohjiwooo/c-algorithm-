07a29a21192d856bf5adf490b92550fb2e523e11
ohjiwooo <64720444+ohjiwooo@users.noreply.github.com>
2020-05-05 22:30:28 +0900

    commit

---
+++ dynamic/fibo.cpp
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<iostream>
+
+using namespace std;
+
+int fibo(int n) {
+
+
+	if (n == 1 || n == 2) {
+		return 1;
+
+	}
+	else return (fibo(n - 1) + fibo(n - 2));
+
+}
+
+
+int main() {
+	int n;
+
+
+	printf("n: ");
+	cin >> n;
+	printf("\n´ä:%d", fibo(n));
+
+}
