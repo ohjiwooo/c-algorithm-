1d3cf15fdb507837f89349faed251b203c1f6928
ohjiwooo <64720444+ohjiwooo@users.noreply.github.com>
2020-05-08 16:30:23 +0900

    commit

--- code/problem1.cpp
+++ code/problem1.cpp
@@ -7,24 +7,56 @@ using namespace std;
 
 string str;
 
+int fun_1(int n) {
+	int temp = 0;
+	int ans = 0;
+//	int i = 8;
+	int flag = 0;
+	for (int i = 1; i <= n; i++) {
+		int start = 0;
+		while ((start + (2 * i) - 1) <= n) {
+			string cut = str.substr(start, i);
+			start = start + i;
+			string cut_2 = str.substr(start, i);
+			if (cut.compare(cut_2) == 0) {
+				flag = 1;
+			}
+			else {
+				if (flag == 1) {
+					temp = temp + 1 + i;
+					flag = 0;
+				}
+				else {
+					temp = temp + i;
+					flag = 0;
+				}
+
+			}
+		}
+		if (flag == 1) {
+			temp = temp + 1 + i;
+			temp = temp + (n - (start + i));
+		}
+		else { temp = temp + (n - start); }
+
+		if (ans == 0) {
+			ans = temp; temp = 0; }
+		else {
+			if (temp <= ans) { 
+				ans = temp; temp = 0; }
+			temp = 0;
+		}
+//		cout <<"i:"<< i <<"ans:"<< ans <<"temp:"<<temp <<"\n";
+	}
+	return ans;
+}
+
+
+
 int main() {
 
 	cin >> str;
-	int n = str.length; //문자열길이
-	int num; //동일문자열개수
-	int start=0; //시작문자열
-	string ans; 
-
-	for (int i = 1; 1 < n;i++) {
-
-		string cut=str.substr(start,(start+i));
-		start = start + i;
-		string cut_2 = str.substr(start, (start + i));
-		if (cut.compare(cut_2) == 0) {
-			
-		}
-
-	}
-
+	int n = str.length();
+	cout << fun_1(n);
 	return 0;
 }
