ec533ce83bb402e973f94d7b045243456c3c728a
ohjiwooo <64720444+ohjiwooo@users.noreply.github.com>
2020-05-13 21:59:49 +0900

    commit

--- baekjoon/n7576.cpp
+++ baekjoon/n7576.cpp
@@ -8,25 +8,18 @@ int n;
 int m;
 int ans=0;
 int a;//안익은거 수
-int k;
+int k=0;
 int cc = 2;
+int qq = 1;
+int qq2=0;
 void change(int i, int j) {
-	if (j != m - 1 && box[i][j + 1] != 0&& box[i][j + 1] != cc&& box[i][j + 1]!=-1) { box[i][j] = cc; a--; }//오른쪽
-	else if (j != 0 && box[i][j - 1] != 0 && box[i][j - 1] != cc&& box[i][j - 1] != -1) { box[i][j] = cc; a--; }//왼쪽
-	else if (i != 0 && box[i - 1][j] !=0 && box[i-1][j] != cc && box[i - 1][j] != -1) { box[i][j] = cc; a--; }//위
-	else if (i != n - 1 && box[i + 1][j] !=0 &&box[i + 1][j] !=cc && box[i + 1][j] != -1) { box[i][j] = cc; a--; }//아래
+	if (j != m - 1 && box[i][j + 1] != 0 && box[i][j + 1] != cc && box[i][j + 1] != -1) { box[i][j] = cc; a--; qq++; }//오른쪽
+	else if (j != 0 && box[i][j - 1] != 0 && box[i][j - 1] != cc && box[i][j - 1] != -1) { box[i][j] = cc; a--; qq++; }//왼쪽
+	else if (i != 0 && box[i - 1][j] != 0 && box[i - 1][j] != cc && box[i - 1][j] != -1) { box[i][j] = cc; a--; qq++; }//위
+	else if (i != n - 1 && box[i + 1][j] != 0 && box[i + 1][j] != cc && box[i + 1][j] != -1) { box[i][j] = cc; a--; qq++; }//아래
 }
 
-/*void change2() {
 
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++) {
-			if (c[i][j] == 1) { box[i][j] = 1; }
-		}
-
-	}
-
-}*/
 int main() {
 
 	cin >> m; //가로칸
@@ -42,21 +35,23 @@ int main() {
 	if (a == 0) { cout << "0"; return 0; }
 
 
-	for ( k = 0; k < 3000; k++) {
+	while (a!=0) {
 		for (int i = 0; i < n; i++) {
 			for (int j = 0; j < m; j++) {
 				if (box[i][j] == 0) {
 					change(i, j);
 				}
-			}	
-		}cc++;//change2();
-		if (a == 0) { break; }
+			}
+		}cc++; if (qq2 == qq) { break; }
+		else { qq2 = qq; }
+		k++;
+		
 	}
 
 	
 	if (a != 0) { cout << "-1"; return 0; }
 
 
-	cout << k+1;
+	cout << k;
 	return 0;
 }
