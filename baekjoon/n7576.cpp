e2bee79084924538bb333a243beecb4cb2606ca1
ohjiwooo <64720444+ohjiwooo@users.noreply.github.com>
2020-05-13 22:16:41 +0900

    commit

--- baekjoon/n7576.cpp
+++ baekjoon/n7576.cpp
@@ -1,5 +1,5 @@
 #include<iostream>
-
+#include<algorithm>
 using namespace std;
 
 int box[1002][1002];
@@ -9,14 +9,16 @@ int m;
 int ans=0;
 int a;//안익은거 수
 int k=0;
-int cc = 2;
-int qq = 1;
-int qq2=0;
-void change(int i, int j) {
-	if (j != m - 1 && box[i][j + 1] != 0 && box[i][j + 1] != cc && box[i][j + 1] != -1) { box[i][j] = cc; a--; qq++; }//오른쪽
-	else if (j != 0 && box[i][j - 1] != 0 && box[i][j - 1] != cc && box[i][j - 1] != -1) { box[i][j] = cc; a--; qq++; }//왼쪽
-	else if (i != 0 && box[i - 1][j] != 0 && box[i - 1][j] != cc && box[i - 1][j] != -1) { box[i][j] = cc; a--; qq++; }//위
-	else if (i != n - 1 && box[i + 1][j] != 0 && box[i + 1][j] != cc && box[i + 1][j] != -1) { box[i][j] = cc; a--; qq++; }//아래
+int day[1002] = { 0, };
+int dayn = 0;
+void change(int i, int j, int dayn) {
+	day[dayn]++;
+	c[i][j] = 1;
+	a--;
+	if (j != m - 1 && box[i][j + 1] == 0 && c[i][j + 1] != 1) { change(i, j + 1,dayn); }//오른쪽
+	else if (j != 0 && box[i][j - 1] == 0 && c[i][j - 1] != 1 ) { change(i, j - 1,dayn); }//왼쪽
+	else if (i != 0 && box[i - 1][j] == 0 &&c[i - 1][j] !=1 ) { change(i-1, j,dayn); }//위
+	else if (i != n - 1 && box[i + 1][j] == 0 && c[i + 1][j] != 1 ) { change(i+1,j,dayn); }//아래
 }
 
 
@@ -35,23 +37,20 @@ int main() {
 	if (a == 0) { cout << "0"; return 0; }
 
 
-	while (a!=0) {
+	
 		for (int i = 0; i < n; i++) {
 			for (int j = 0; j < m; j++) {
-				if (box[i][j] == 0) {
-					change(i, j);
+				if (box[i][j] == 1) {
+					change(i, j,dayn++);
 				}
 			}
-		}cc++; if (qq2 == qq) { break; }
-		else { qq2 = qq; }
-		k++;
+		}
+		if (a != 0) { cout << "-1"; return 0; }
+		sort(day[0], day[dayn - 1]);
 		
-	}
-
 	
-	if (a != 0) { cout << "-1"; return 0; }
 
 
-	cout << k;
+	cout << day[0];
 	return 0;
 }
