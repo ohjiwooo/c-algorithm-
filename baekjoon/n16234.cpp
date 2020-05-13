6bbef732fc2a0ad17a99fa74200aaaf66ddc94b3
ohjiwooo <64720444+ohjiwooo@users.noreply.github.com>
2020-05-13 20:57:43 +0900

    commit

--- baekjoon/n16234.cpp
+++ baekjoon/n16234.cpp
@@ -2,16 +2,16 @@
 #include<algorithm>
 using namespace std;
 
-int ground[50][50] = { 0, }; //인구수 넣기
-int check[50][5] = { 0, };//국경선 상태 표시
+int ground[52][52] = { 0, }; //인구수 넣기
 int n; //땅크기
 int l; //인구차 이상
 int m; //인구차 이하
-int v[50][50] = { 0, };
-
+int v[52][52] = { 0, };
+int g;
 int mm = 0;//합
 int nn = 0;//개수
 int vn = 1;
+
 int p(int a) {
 	if (a >= 0) { return a; }
 	else { return - a; }
@@ -22,17 +22,17 @@ void cc(int i, int j) {
 	v[i][j] = vn;
 	mm = mm + ground[i][j];
 	nn++;
-	cout << "i:"<<i<<"  j:"<<j<<"  nn:" << nn << "  mm:" << mm << "\n";
-	if (j != n - 1&&(p(ground[i][j + 1] - ground[i][j]) >= l) && (p(ground[i][j + 1] - ground[i][j]) <= m)&&v[i][j+1]!=vn) {
+
+	if (j != n - 1&&(p(ground[i][j + 1] - ground[i][j]) >= l) && (p(ground[i][j + 1] - ground[i][j]) <= m)&&v[i][j+1]==0) {
 		cc(i, j + 1);
 	}//오
-	if (j!=0&&(p(ground[i][j - 1] - ground[i][j]) >= l) && (p(ground[i][j - 1] - ground[i][j]) <= m) && v[i][j - 1] != vn) {
+	if (j!=0&&(p(ground[i][j - 1] - ground[i][j]) >= l) && (p(ground[i][j - 1] - ground[i][j]) <= m) && v[i][j - 1] ==0) {
 		cc(i, j - 1);
 	}//왼
-	if (i != 0&& (p(ground[i - 1][j] - ground[i][j]) >= l) && (p(ground[i - 1][j] - ground[i][j]) <= m) && v[i - 1][j] != vn) {
+	if (i != 0&& (p(ground[i - 1][j] - ground[i][j]) >= l) && (p(ground[i - 1][j] - ground[i][j]) <= m) && v[i - 1][j] ==0) {
 		cc(i-1,j);
 	}//위
-	if (i!=n-1&&(p(ground[i + 1][j] - ground[i][j]) >= l) && (p(ground[i + 1][j] - ground[i][j]) <= m) && v[i+1][j] != vn) {
+	if (i!=n-1&&(p(ground[i + 1][j] - ground[i][j]) >= l) && (p(ground[i + 1][j] - ground[i][j]) <= m) && v[i+1][j] ==0) {
 		cc(i + 1, j);
 	}//아래
 
@@ -55,58 +55,36 @@ int main() {
 	cin >> n;
 	cin >> l;
 	cin >> m;
-	
+
 
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < n; j++) {
 			cin >> ground[i][j];
 		}
 	}
-
 	
-	int g;
-	for (g = 0; g < 2000; g++) {
+	for (g = 0; g < 2001; g++) {
 		vn = 1;
-		fill(&v[0][0], &v[50][50], 0);
-		for (int i = 0; i < n; i++) {
+		fill(&v[0][0], &v[49][49], 0);
+		for (int i = 0; i <n ; i++) {
 			for (int j = 0; j < n; j++) {
+				nn = 0; mm = 0;
 				if (v[i][j] == 0) {
-					nn = 0; mm = 0; cc(i, j); 
+					cc(i, j); 
 					if (nn > 1) { cc2(mm, nn); }
 					vn++;
 				}
 			}
-			
-		}//cout <<"vn:"<<vn<< "  g:" << g << "\n";
-		cout << "-----------------------------------\n";
-		for (int i = 0; i < n ; i++) {
-			for (int j = 0; j < n ; j++) {
-				cout << ground[i][j] << " ";
-			}
-			cout << "\n";
 		}
-
+		cout <<"g:"<<g<< "-----------------------\n";
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				cout << ground[i][j]<<" ";
+			}cout << "\n";
+		}
 		if (vn == (n*n+1)) {break;}
 		vn = 1;
 	}
-
-	
-	cout << g ;
-/*	for (int i = 0; i < (n*n); i++) {
-		for (int j = 1; j < 5; j++) {
-			cout << check[i][j] << " ";
-		}
-		cout << "\n";
-	}*/
-
-
-
-/*	for (int i = 0; i <= n+1; i++) {
-		for (int j = 0; j <= n+1; j++) {
-			cout<< ground[i][j]<<" ";
-		}
-		cout << "\n";
-	}*/
-
+		cout << g;
 	return 0;
 }
