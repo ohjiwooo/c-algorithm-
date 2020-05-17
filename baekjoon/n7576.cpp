c7fa2424f5547873c8f4249c8e5792de2c96d106
ohjiwooo <64720444+ohjiwooo@users.noreply.github.com>
2020-05-17 16:04:10 +0900

    commit

--- baekjoon/n7576.cpp
+++ baekjoon/n7576.cpp
@@ -1,29 +1,28 @@
 #include<iostream>
-#include<algorithm>
+#include<queue>
 using namespace std;
 
 int box[1002][1002];
 int c[1002][1002] = {0,};
 int n;
 int m;
-int ans=0;
 int a;//안익은거 수
-int k=0;
-int day[1002] = { 0, };
-int dayn = 0;
-void change(int i, int j, int dayn) {
-	day[dayn]++;
-	c[i][j] = 1;
-	a--;
-	if (j != m - 1 && box[i][j + 1] == 0 && c[i][j + 1] != 1) { change(i, j + 1,dayn); }//오른쪽
-	else if (j != 0 && box[i][j - 1] == 0 && c[i][j - 1] != 1 ) { change(i, j - 1,dayn); }//왼쪽
-	else if (i != 0 && box[i - 1][j] == 0 &&c[i - 1][j] !=1 ) { change(i-1, j,dayn); }//위
-	else if (i != n - 1 && box[i + 1][j] == 0 && c[i + 1][j] != 1 ) { change(i+1,j,dayn); }//아래
-}
+int ans = 0;
+int i;
+int j;
+int b=0;//익은거수
+typedef struct s{
+
+	int i;
+	int j;
+
+}s;
 
 
 int main() {
-
+	
+	queue<s> qq;
+	int size;
 	cin >> m; //가로칸
 	cin >> n;//세로칸
 
@@ -31,26 +30,40 @@ int main() {
 		for (int j = 0; j < m; j++) {
 			cin >> box[i][j];
 			if (box[i][j] == 0) { a++; }
+			else if (box[i][j] == 1) { qq.push({ i,j }); c[i][j] = 1; b++; }
+			else if (box[i][j] == -1) { c[i][j] = -1; }
 		}
 	} //입력받음
-
-	if (a == 0) { cout << "0"; return 0; }
-
+	
+	if (a == 0) { cout << "0"; return 0; } //0개일 경우 0리턴		
+	a = a + b;
+	
+	while (qq.empty() != true) {
+		size = qq.size();
+		for (int k= 0; k < size; k++) {
+			i = qq.front().i;
+			j = qq.front().j;
+		
+			qq.pop();
+			box[i][j] = 1;
+			a--;
+		
+			if (i != 0 && box[i - 1][j] == 0 && c[i - 1][j] != 1) { qq.push({ i - 1,j }); c[i - 1][j] = 1; } //위
+			if (j != 0 && box[i][j - 1] == 0 && c[i][j - 1] != 1) { qq.push({ i ,j - 1}); c[i][j - 1] = 1; }//왼
+			if (j != m - 1 && box[i][j + 1] == 0 && c[i][j + 1] != 1) { qq.push({ i ,j + 1 }); c[i][j + 1] = 1; }//오
+			if (i != n - 1 && box[i + 1][j] == 0 && c[i + 1][j] != 1) { qq.push({ i + 1 ,j }); c[i + 1][j] = 1; }//아
+		}ans++; 
+		
+	}
 
 	
-		for (int i = 0; i < n; i++) {
-			for (int j = 0; j < m; j++) {
-				if (box[i][j] == 1) {
-					change(i, j,dayn++);
-				}
-			}
-		}
-		if (a != 0) { cout << "-1"; return 0; }
-		sort(day[0], day[dayn - 1]);
+	if (a != 0) { cout << "-1"; return 0; }
+	else { cout << ans-1; }
+		
 		
 	
 
 
-	cout << day[0];
+		
 	return 0;
 }
