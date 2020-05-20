03e2c35000a881fc44002d2089751b1f640b003e
ohjiwooo <64720444+ohjiwooo@users.noreply.github.com>
2020-05-21 00:51:32 +0900

    commit

--- baekjoon/n17143.cpp
+++ baekjoon/n17143.cpp
@@ -3,10 +3,9 @@
 using namespace std;
 
 typedef struct s{
-	int s=0; //속력
-	int d=0; //방향
-	int z=0; //크키
-	
+	int s; //속력
+	int d; //방향
+	int z; //크키
 }s;
 
 int r, c,m;
@@ -14,115 +13,34 @@ s map[102][102];
 int answer = 0;
 s temp[102][102];
 
+void change(int a,int b,int c,int d) {
+	if (temp[a][b].z < map[c][d].z) { //이미있는 애가 없거나 더 작음
+		temp[a][b].s = map[c][d].s;
+		temp[a][b].d = map[c][d].d;
+		temp[a][b].z = map[c][d].z;
+	}
 
+}
 
 
 void move() {
-	for (int i = 0; i < r; i++) {
-		for (int j = 0; j < c; j++) {
+	for (int i = 1; i < r; i++) {
+		for (int j = 1; j < c; j++) {
 			if (map[i][j].d == 1) {//위
-				if (i - map[i][j].s > 0) {//범위안넘음
-					if (temp[i - map[i][j].s][j].z < map[i][j].z) { //이미있는 애가 없거나 더 작음
-						temp[i - map[i][j].s][j].s = map[i][j].s;
-						temp[i - map[i][j].s][j].d = map[i][j].d;
-						temp[i - map[i][j].s][j].z = map[i][j].z;
-					}
-				}
-				else if (i - map[i][j].s == 0) {
-					map[i][j].d = 2;
-					if (temp[0][j].z < map[i][j].z) { //이미있는 애가 없거나 더 작음
-						temp[0][j].s = map[i][j].s;
-						temp[0][j].d = map[i][j].d;
-						temp[0][j].z = map[i][j].z;
-					}
-				}
-				else {//범위넘음
-					map[i][j].d = 2;//방향바꿈
-					int n = (i + map[i][j].s) % r+2; //인덱스 계산
-					if (temp[n][j].z < map[i][j].z) { //이미있는 애가 없거나 더 작음
-						temp[n][j].s = map[i][j].s;
-						temp[n][j].d = map[i][j].d;
-						temp[n][j].z = map[i][j].z;
-					}
-				}
+				
+
+
 			}
 			else if (map[i][j].d == 2) {//아래
-				if (i + map[i][j].s <r-1) {//범위안넘음
-					if (temp[i + map[i][j].s][j].z < map[i][j].z) { //이미있는 애가 없거나 더 작음
-						temp[i + map[i][j].s][j].s = map[i][j].s;
-						temp[i + map[i][j].s][j].d = map[i][j].d;
-						temp[i + map[i][j].s][j].z = map[i][j].z;
-					}
-				}
-				else if (i - map[i][j].s == r-1) { //마지막에있음
-					map[i][j].d = 1;
-					if (temp[r-1][j].z < map[i][j].z) { //이미있는 애가 없거나 더 작음
-						temp[r-1][j].s = map[i][j].s;
-						temp[r-1][j].d = map[i][j].d;
-						temp[r-1][j].z = map[i][j].z;
-					}
-				}
-				else {//범위넘음
-					map[i][j].d = 1;//방향바꿈
-					int n =r- ((i + map[i][j].s) % r+2); //인덱스 계산
-					if (temp[n][j].z < map[i][j].z) { //이미있는 애가 없거나 더 작음
-						temp[n][j].s = map[i][j].s;
-						temp[n][j].d = map[i][j].d;
-						temp[n][j].z = map[i][j].z;
-					}
-				}
+				
 			}
 			else if (map[i][j].d == 3) {//오른쪽
-				if (j + map[i][j].s < c-1) {//범위안넘음
-					if (temp[i][j + map[i][j].s].z < map[i][j].z) { //이미있는 애가 없거나 더 작음
-						temp[i][j + map[i][j].s].s = map[i][j].s;
-						temp[i][j + map[i][j].s].d = map[i][j].d;
-						temp[i][j + map[i][j].s].z = map[i][j].z;
-					}
-				}
-				else if (j + map[i][j].s == c-1) {
-					map[i][j].d = 4;
-					if (temp[0][c-1].z < map[i][j].z) { //이미있는 애가 없거나 더 작음
-						temp[0][c-1].s = map[i][j].s;
-						temp[0][c-1].d = map[i][j].d;
-						temp[0][c-1].z = map[i][j].z;
-					}
-				}
-				else {//범위넘음
-					map[i][j].d = 4;//방향바꿈
-					int n = c - ((j + map[i][j].s) % c+2); //인덱스 계산
-					if (temp[n][j].z < map[i][j].z) { //이미있는 애가 없거나 더 작음
-						temp[n][j].s = map[i][j].s;
-						temp[n][j].d = map[i][j].d;
-						temp[n][j].z = map[i][j].z;
-					}
-				}
+				
+		
 			}
 			else if (map[i][j].d == 4) {//왼쪽
-				if (j - map[i][j].s > 0) {//범위안넘음
-					if (temp[i][j - map[i][j].s].z < map[i][j].z) { //이미있는 애가 없거나 더 작음
-						temp[i][j - map[i][j].s].s = map[i][j].s;
-						temp[i][j - map[i][j].s].d = map[i][j].d;
-						temp[i][j - map[i][j].s].z = map[i][j].z;
-					}
-				}
-				else if (i - map[i][j].s == 0) {
-					map[i][j].d = 3;
-					if (temp[i][0].z < map[i][j].z) { //이미있는 애가 없거나 더 작음
-						temp[i][0].s = map[i][j].s;
-						temp[i][0].d = map[i][j].d;
-						temp[i][0].z = map[i][j].z;
-					}
-				}
-				else {//범위넘음
-					map[i][j].d = 3;//방향바꿈
-					int n = (i + map[i][j].s) % c+2; //인덱스 계산
-					if (temp[i][n].z < map[i][j].z) { //이미있는 애가 없거나 더 작음
-						temp[i][n].s = map[i][j].s;
-						temp[i][n].d = map[i][j].d;
-						temp[i][n].z = map[i][j].z;
-					}
-				}
+				
+				
 			}
 		}
 	}
@@ -144,21 +62,23 @@ void update() {
 }
 
 int main() {
-
+	int a, b, c, d, e;
 	cin >> r >> c >> m;
-
-	int a, b;
-
-	for (int i = 0; i < r; i++) {
-		for (int j = 0; j < c; j++) {
-			cin >> a;
-			cin >> b;
-			temp[i][j].z = 0;
-			if (i == a && j == b) {
-				cin >> map[i][j].d;
-				cin >> map[i][j].s;
-				cin >> map[i][j].z;
-
+	
+	for (int k = 0; k <r*m; k++) {
+		cin >> a;
+		cin >> b;
+		cin >> c;
+		cin >> d;
+		cin >> e;
+		for (int i = 1; i <= r; i++) {
+			for (int j = 1; j <= c; j++) {
+				if (i == a && j == b) {
+					map[i][j].d = c;
+					map[i][j].s=d;
+					map[i][j].z=e;
+				}
+				else { temp[i][j].z = 0; }
 			}
 		}
 	}
