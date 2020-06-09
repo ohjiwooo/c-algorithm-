31720f104f6d6bf280716534d95fc5eae5173553
ohjiwooo <64720444+ohjiwooo@users.noreply.github.com>
2020-06-09 18:08:10 +0900

    commit

--- baekjoon/n9663.cpp
+++ baekjoon/n9663.cpp
@@ -3,9 +3,43 @@
 using namespace std;
 
 
+int q[20] = { 0 };
+int n;
+int ans = 0;
+
+int p(int a) {
+	if (a < 0) { return -a; }
+	else { return a; }
+}
+
+bool pos(int row) {
+
+	for (int i = 1; i < row; i++) {
+		if (q[i]==q[row] || (p(i-row)==p(q[i]-q[row]))) {//못놓는 경우
+			return false;
+		}
+		else { return true; }
+	
+	}
+}
+
+int backtracking(int row) { //현재로우,입력받은 n
+
+	for (int i = 1; i <= n; i++) {
+	
+		q[row] = i;
+		if (pos(i) == true) {
+			backtracking(++row);
+		}
+	
+	}
+
+}
+
 int main() {
 
-
+	cin >> n;
+	backtracking(1);
 
 	return 0;
 }
