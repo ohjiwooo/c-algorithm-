75bbccd3b1dfdbfe5aa89f30ee5ddab0b71de1ac
ohjiwooo <64720444+ohjiwooo@users.noreply.github.com>
2020-06-19 23:51:38 +0900

    commit

--- baekjoon/n3987.cpp
+++ baekjoon/n3987.cpp
@@ -3,48 +3,128 @@
 using namespace std;
 
 int n, m;
-int map[505][505];
+char map[505][505];
 int pr, pc;
 int ans = 0;
 char answer;
+int answ = 0;
+char aa;
 
-void up(int i2, int j2) {
-	for (int i = 0; i < n;i++) {
-		for (int j = 0; j < m;j++) {
-			if (map[i2 - 1][j] == '.') {
-				ans++;
-			}
-			else if (map[i2 - 1][j] == 'C') {
-				return;
-			}
-			else if (map[i2 - 1][j] == '/') {
-				return;
-			}
+void u(int i, int j);
+void d(int i, int j);
+void r(int i, int j);
+void l(int i, int j);
+
+void u(int i, int j) {
+	
+	while (i>0) {
+		i--;
+
+		if (map[i][j] == '.') {
+			ans++;
+		}
+		else if (map[i][j] == 'C') {
+			return;
+		}
+		else if (map[i][j] == 92) {
+			ans++;
+			if (aa == 'l') {cout}
+			aa = 'u';
+			l(i, j);
+			return;
+		}
+		else if (map[i][j] == '/') {
+			ans++;
+			aa = 'u';
+			r(i, j);
+			return;
 		}
 	}
 }
 
-void right(int i2, int j2) {
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++) {
+void r(int i, int j) {
+	
+	while (j <= m) {
+		
+		j++;
 
+		if (map[i][j] == '.') {
+			ans++;
 		}
+		else if (map[i][j] == 'C') {
+			return;
+		}
+		else if (map[i][j] == 92) {
+			ans++;
+			aa = 'r';
+			d(i, j);
+			return;
+		}
+		else if (map[i][j] == '/') {
+			ans++;
+			aa = 'r';
+			u(i, j);
+			return;
+		}
+
+
 	}
 }
 
-void down(int i2, int j2) {
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++) {
+void d(int i, int j) {
+	
+	while (i <= n) {
 
+		i++;
+
+		if (map[i][j] == '.') {
+			ans++;
 		}
+		else if (map[i][j] == 'C') {
+			return;
+		}
+		else if (map[i][j] == 92) {
+			ans++;
+			aa = 'd';
+			r(i, j);
+			return;
+		}
+		else if (map[i][j] == '/') {
+			ans++;
+			aa = 'd';
+			l(i, j);
+			return;
+		}
+
+
 	}
 }
 
-void left(int i2, int j2) {
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++) {
+void l(int i, int j) {
+	while (j > 0) {
 
+		j--;
+
+		if (map[i][j] == '.') {
+			ans++;
 		}
+		else if (map[i][j] == 'C') {
+			return;
+		}
+		else if (map[i][j] == 92) {
+			ans++;
+			aa = 'l';
+			u(i, j);
+			return;
+		}
+		else if (map[i][j] == '/') {
+			ans++;
+			aa = 'l';
+			d(i, j);
+			return;
+		}
+
+
 	}
 }
 
@@ -52,14 +132,44 @@ int main() {
 
 	cin >> n >> m;
 
-	for (int i = 0; i < n;i++) {
-		for (int j = 0; j < m; j++) {
+	for (int i = 1; i <= n;i++) {
+		for (int j = 1; j <= m; j++) {
 			cin >> map[i][j];
 		}
 	}
 	
 	cin >> pr >> pc;
 
+	u(pr, pc);
+	if (ans > answ) {
+		answ = ans;
+		answer = 'U';
+	}
+	ans = 0;
+
+	l(pr, pc);
+	if (ans > answ) {
+		answ = ans;
+		answer = 'L';
+	}
+	ans = 0;
+
+	d(pr, pc);
+	if (ans > answ) {
+		answ = ans;
+		answer = 'D';
+	}
+	ans = 0;
+
+	l(pr, pc);
+	if (ans > answ) {
+		answ = ans;
+		answer = 'L';
+	}
+	
+
+	cout << answer << "\n";
+	cout << ++answ;
 
 	return 0;
 }
