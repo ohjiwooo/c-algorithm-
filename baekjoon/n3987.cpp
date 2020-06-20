00c1234b4cd03a1526137cdb4b948d5f48e890a9
ohjiwooo <64720444+ohjiwooo@users.noreply.github.com>
2020-06-20 23:09:33 +0900

    commit

--- baekjoon/n3987.cpp
+++ baekjoon/n3987.cpp
@@ -2,13 +2,24 @@
 
 using namespace std;
 
+
+typedef struct s {
+
+	bool u = false;
+	bool d = false;
+	bool r = false;
+	bool l = false;
+
+}s;
+
 int n, m;
 char map[505][505];
 int pr, pc;
 int ans = 0;
 char answer;
 int answ = 0;
-
+int flag = 0;
+s vis[505][505];
 
 
 void u(int i, int j);
@@ -23,17 +34,38 @@ void u(int i, int j) {
 
 		if (map[i][j] == '.') {
 			ans++;
+			if (vis[i][j].u == true) {
+				cout << "U\n";
+				cout << "Voyager";
+				flag = 1;
+				return;
+			}
+			vis[i][j].u = true;
 		}
 		else if (map[i][j] == 'C') {
 			return;
 		}
 		else if (map[i][j] == 92) {
 			ans++;
+			if (vis[i][j].u == true) {
+				cout << "U\n";
+				cout << "Voyager";
+				flag = 1;
+				return;
+			}
+			vis[i][j].u = true;
 			l(i, j);
 			return;
 		}
 		else if (map[i][j] == '/') {
 			ans++;
+			if (vis[i][j].u == true) {
+				cout << "U\n";
+				cout << "Voyager";
+				flag = 1;
+				return;
+			}
+			vis[i][j].u = true;
 			r(i, j);
 			return;
 		}
@@ -48,17 +80,38 @@ void r(int i, int j) {
 
 		if (map[i][j] == '.') {
 			ans++;
+			if (vis[i][j].r == true) {
+				cout << "R\n";
+				cout << "Voyager";
+				flag = 1;
+				return;
+			}
+			vis[i][j].r = true;
 		}
 		else if (map[i][j] == 'C') {
 			return;
 		}
 		else if (map[i][j] == 92) {
 			ans++;
+			if (vis[i][j].r == true) {
+				cout << "R\n";
+				cout << "Voyager";
+				flag = 1;
+				return;
+			}
+			vis[i][j].r = true;
 			d(i, j);
 			return;
 		}
 		else if (map[i][j] == '/') {
 			ans++;
+			if (vis[i][j].r == true) {
+				cout << "R\n";
+				cout << "Voyager";
+				flag = 1;
+				return;
+			}
+			vis[i][j].r = true;
 			u(i, j);
 			return;
 		}
@@ -75,17 +128,38 @@ void d(int i, int j) {
 
 		if (map[i][j] == '.') {
 			ans++;
+			if (vis[i][j].d == true) {
+				cout << "D\n";
+				cout << "Voyager";
+				flag = 1;
+				return;
+			}
+			vis[i][j].d = true;
 		}
 		else if (map[i][j] == 'C') {
 			return;
 		}
 		else if (map[i][j] == 92) {
 			ans++;
+			if (vis[i][j].d == true) {
+				cout << "D\n";
+				cout << "Voyager";
+				flag = 1;
+				return;
+			}
+			vis[i][j].d = true;
 			r(i, j);
 			return;
 		}
 		else if (map[i][j] == '/') {
 			ans++;
+			if (vis[i][j].d == true) {
+				cout << "D\n";
+				cout << "Voyager";
+				flag = 1;
+				return;
+			}
+			vis[i][j].d = true;
 			l(i, j);
 			return;
 		}
@@ -101,17 +175,38 @@ void l(int i, int j) {
 
 		if (map[i][j] == '.') {
 			ans++;
+			if (vis[i][j].l == true) {
+				cout << "L\n";
+				cout << "Voyager";
+				flag = 1;
+				return;
+			}
+			vis[i][j].l = true;
 		}
 		else if (map[i][j] == 'C') {
 			return;
 		}
 		else if (map[i][j] == 92) {
 			ans++;
+			if (vis[i][j].l == true) {
+				cout << "L\n";
+				cout << "Voyager";
+				flag = 1;
+				return;
+			}
+			vis[i][j].l = true;
 			u(i, j);
 			return;
 		}
 		else if (map[i][j] == '/') {
 			ans++;
+			if (vis[i][j].l == true) {
+				cout << "L\n";
+				cout << "Voyager";
+				flag = 1;
+				return;
+			}
+			vis[i][j].l = true;
 			d(i, j);
 			return;
 		}
@@ -132,37 +227,47 @@ int main() {
 	
 	cin >> pr >> pc;
 
-	u(pr, pc);
+	while (1) {
+		u(pr, pc);
+		if (flag == 1) { return 0; }
+
+		if (ans > answ) {
+			answ = ans;
+			answer = 'U';
+		}
+		ans = 0;
+		
+
+		r(pr, pc);
+		if (flag == 1) { return 0; }
+
+		if (ans > answ) {
+			answ = ans;
+			answer = 'R';
+		}
+		ans = 0;
+		
+		d(pr, pc);
+		if (flag == 1) { return 0; }
+
+		if (ans > answ) {
+			answ = ans;
+			answer = 'D';
+		}
+		ans = 0;
 	
-	if (ans > answ) {
-		answ = ans;
-		answer = 'U';
+
+		l(pr, pc);
+		if (flag == 1) { return 0; }
+
+		if (ans > answ) {
+			answ = ans;
+			answer = 'L';
+		}
+		break;
 	}
-	ans = 0;
 
-
-	r(pr, pc);
-	if (ans > answ) {
-		answ = ans;
-		answer = 'R';
-	}
-	ans = 0;
-
-	d(pr, pc);
-	if (ans > answ) {
-		answ = ans;
-		answer = 'D';
-	}
-	ans = 0;
-
-	l(pr, pc);
-
-	if (ans > answ) {
-		answ = ans;
-		answer = 'L';
-	}
 	
-
 	cout << answer << "\n";
 	cout << ++answ;
 
