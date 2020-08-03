73b19b2c2e2a11bd0260f2eddcf642cd77afce5b
ohjiwooo <64720444+ohjiwooo@users.noreply.github.com>
2020-08-04 00:45:06 +0900

    commit

--- baekjoon/n17140.cpp
+++ baekjoon/n17140.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<algorithm>
+#include<vector>
 using namespace std;
 
 int t=0;
@@ -7,83 +8,101 @@ int r, c, k;
 int arr[105][105] = { 0 };
 int x = 3;
 int y = 3;
+typedef struct s {
+	int sum;
+	int n;
 
+}s;
+bool comp(s a,s b) {
+
+	return a.sum < b.sum || ((a.sum == b.sum) && a.n < b.n);
+
+}
 void func_r() {
 	int new_y=y;
 
-	for (int i = 1; i <= x;i++) {
-
+	for (int i = 1; i <= x; i++) {
+		vector <s> v;
 		int temp[105] = { 0 };
 		int temp2 = 1;
 
-		for (int j = 1; j <=y;j++) {
+		for (int j = 1; j <= y; j++) {
 			temp[arr[i][j]]++;
 		}
-		for (int z = 1; z < 101;z++) {
-			if (temp[z]!=0 && temp2<=101) {
-				arr[i][temp2++] = z;
-				if (temp2<=101) {
-					arr[i][temp2++] = temp[z];
-				}
-				else {
-					break;
-				}
-			}
-			else if (temp2 > 101) { break; }
-			
-		}if (--temp2<new_y && i!=1) {
-			for (int z = temp2+1; z <= new_y;z++) {
+		for (int j = 1; j <= 100; j++) {
+			if (temp[j] != 0) { v.push_back({ temp[j],j }); }
+		}
+
+		sort(v.begin(), v.end(), comp);
+		
+		 for (int j = 0; j < v.size(); j++) {
+			 if (temp2 <= 100) {
+				 arr[i][temp2++] = v[j].n;
+			 }
+			 else { break; }
+			 if (temp2 <= 100) {
+				 arr[i][temp2++] = v[j].sum;
+			 }
+			 else { break; }
+		}
+		--temp2;
+		if (i == 1) { new_y = temp2; }
+		if (temp2 < y) {
+			for (int z = temp2 + 1; z <= y; z++) {
 				arr[i][z] = 0;
 			}
+			if (new_y < temp2) { new_y = temp2; }
 		}
-		else if (i == 1) { new_y = temp2; }
-		else if(temp2>new_y){ new_y = temp2; }
+		else {
+			if (new_y < temp2) { new_y= temp2; }
+		}
+	
 	}
 	y = new_y;
+		
 }
 void func_c(){
-
 	int new_x = x;
 
 	for (int j = 1; j <= y; j++) {
-
-		int temp[101] = { 0 };
+		vector <s> v;
+		int temp[105] = { 0 };
 		int temp2 = 1;
 
 		for (int i = 1; i <= x; i++) {
 			temp[arr[i][j]]++;
 		}
-		for (int z = 1; z < 101; z++) {
-			if (temp[z] != 0 && temp2<=101) {
-				arr[temp2++][j] = z;
-				if (temp2<=101) {
-					arr[temp2++][j] = temp[z];
-				}
-				else { break; }
-			}
-			else if (temp2 > 101) { break; }
+		for (int i = 1; i <= 100; i++) {
+			if (temp[i] != 0) { v.push_back({ temp[i],i }); }
+		}
 
-		}if (--temp2 < new_x && j!=1) {
-			for (int z = temp2+1; z <= new_x; z++) {
+		sort(v.begin(), v.end(), comp);
+
+		for (int i = 0; i < v.size(); i++) {
+			if (temp2 <= 100) {
+				arr[temp2++][j] = v[i].n;
+			}
+			else { break; }
+			if (temp2<=100) {
+				arr[temp2++][j] = v[i].sum;
+			}
+			else { break; }
+		}
+		--temp2;
+		if (j == 1) { new_x = temp2; }
+		if (temp2<x) {
+			for (int z = temp2 + 1; z <= x; z++) {
 				arr[z][j] = 0;
 			}
+			if (new_x < temp2) { new_x = temp2; }
 		}
-		else if (j == 1) { new_x = temp2; }
-		else if (temp2 > new_x) { new_x = temp2; }
+		else {
+			if (new_x < temp2) { new_x = temp2; }
+		}
 	}
 	x = new_x;
-
 }
 
-void p() {
-
-	cout << "======================== t: " << t << "=======================\n";
-	for (int i = 1; i <= x; i++) {
-		for (int j = 1; j <= y; j++) {
-			cout << arr[i][j] << " ";
-		}cout << "\n";
-	}
-}
 bool ans() {
 	if (arr[r][c]==k) {
 		return true;
@@ -99,15 +118,7 @@ int main() {
 		}
 	}
 
-/*	func_r();
-	func_c();
-	for (int i = 1; i <= x; i++) {
-		for (int j = 1; j <= y; j++) {
-			cout << arr[i][j]<<" ";
-		}cout << "\n";
-	}*/
 	while (t<=100) {
-		p();
 		if (ans() == true) {
 			cout << t;
 			return 0;
