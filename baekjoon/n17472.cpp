f8e65baf751074d005374cbbc9e4ad622d359ba3
ohjiwooo <64720444+ohjiwooo@users.noreply.github.com>
2020-08-14 17:25:39 +0900

    commit

--- baekjoon/n17472.cpp
+++ baekjoon/n17472.cpp
@@ -43,9 +43,9 @@ int numbering() {
 }
 
 void ff(int a,int b,int vis[15][15]) {
-
+	
 	queue<pair<int, int>> qq;
-
+	queue<pair<int, int>> qq2;
 	qq.push(make_pair(a, b));
 	vis[a][b] = true;
 
@@ -55,8 +55,40 @@ void ff(int a,int b,int vis[15][15]) {
 		int nj = qq.front().second;
 		qq.pop();
 
-
+		if (map[ni+imap[0]][nj+jmap[0]]==0 || map[ni + imap[1]][nj + jmap[1]] == 0 || map[ni + imap[2]][nj + jmap[2]] == 0 || 
+			map[ni + imap[3]][nj + jmap[3]] == 0 ) {
+			qq2.push(make_pair(ni, nj));//观 何盒老版快
+		}
+		else {//酒匆版快
+			for (int i = 0; i < 4; i++) {
+				int newi = ni + imap[i];
+				int newj = nj + jmap[i];
+				if (newi >= 0 && newi < n && newj >= 0 && newj < m && map[newi][newj] != 0 && vis[newi][newj]!=true) {
+					qq.push(make_pair(newi, newj));
+					vis[newi][newj] = true;
+				}
+			}
+		}
+	}
 	
+	int arr[150] = {150};
+	while (qq2.empty() != true) {
+		int ni = qq2.front().first;
+		int nj = qq2.front().second;
+
+		for (int i = 0; i < 4;i++) {
+			int a = ni;
+			int b = nj;
+			int d = 0;
+			while (a+imap[i] >= 0 && a+imap[i] < n && b+jmap[i]>=0 && b+jmap[i] <m) {
+				if (map[a+imap[i]][b+jmap[i]] != 0) {
+					if (map[a + imap[i]][b + jmap[i]] > d ) {
+						map[a + imap[i]][b + jmap[i]] = d;
+					}
+				}
+				d++;
+			}
+		}
 	}
 
 }
