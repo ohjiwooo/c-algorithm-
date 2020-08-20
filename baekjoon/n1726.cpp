3cf3af50242784245595b9087324086699922d2e
ohjiwooo <64720444+ohjiwooo@users.noreply.github.com>
2020-08-20 15:39:32 +0900

    commit

--- baekjoon/n1726.cpp
+++ baekjoon/n1726.cpp
@@ -12,60 +12,60 @@ typedef struct s {
 
 int m,n;
 int map[150][150];
-int ni, nj, nd;
-int ansi, ansj, ansd;
+int ni, nj, nd,ansi, ansj, ansd;
 int imap[5] = {0,0,0,1,-1};
 int jmap[5] = {0,1,-1,0,0};
-bool vis[150][150][5];
+bool vis[150][150][6];
 
 void bfs() {
 
 	queue <s> t;
 	queue <s> g;
 	t.push({ ni,nj,nd,0 });
-	g.push({ ni,nj,nd,0 });
-	int ts=t.size();
+	int ss = 0;
 	while (1) {
-		while(g.empty()!=true) {
-			int i = g.front().i;
-			int j = g.front().j;
-			int d = g.front().d;
-			int a = g.front().ans;
-			g.pop();
-			int k = 1;
-			int temp;
-			vis[i][j][d] = true;
-			while (i + imap[d] * k > 0 && i + imap[d] * k <= m && j + jmap[d] * k > 0 && j + jmap[d] * k <= n && map[i + imap[d] * k][j + jmap[d] * k] == 0) {
-				t.push({ i + imap[d] * k,j + jmap[d] * k,d,a + 1 });
-				k++;
-			}
-		}
-		for (int k = 0; k < ts; k++) {
+
+		while (t.empty()!=true) {
 			int i = t.front().i;
 			int j = t.front().j;
 			int d = t.front().d;
 			int a = t.front().ans;
 			t.pop();
-
+			if (i == ansi && j == ansj && d == ansd) { cout << a; return; }
 			for (int k = 1; k <= 4; k++) {
-				if (i == ansi && j == ansj && k == ansd && k==d) { 
-					cout << a; return; }
-				else if (i == ansi && j == ansj && k == ansd && ((d == 1 && k == 2) || (d == 2 && k == 1) || (d == 3 && k == 4) || (d == 4 && k == 3))) {
-					cout << a+2; return;
-				}
-				else if (i == ansi && j == ansj && k == ansd && k != d) { 
-					cout << a+1; return; }
-
-				if (i + imap[k] > 0 && i + imap[k] <= m && j + jmap[k] > 0 && j + jmap[k] <= n && map[i + imap[k]][j + jmap[k]] == 0 && vis[i][j][k] != true) {
+				if (vis[i][j][k] != true) {
 					vis[i][j][k] = true;
-					if ((d == 1 && k == 2) || (d == 2 && k == 1) || (d == 3 && k == 4) || (d == 4 && k == 3)) {
-						t.push({ i, j, k, a+1 });
-						vis[i][j][k] = false;
+					if (d==k) {
+						g.push({ i, j, k, a });
+					}
+					else if ((d == 1 && k == 2) || (d == 2 && k == 1) || (d == 3 && k == 4) || (d == 4 && k == 3)) {
+						g.push({ i, j, k, a + 2 });
 					}
 					else { g.push({ i, j, k, a + 1 }); }
 				}
 			}
-		}ts = t.size();
+		}
+		int size = g.size();
+		for (int nn = 0; nn < size; nn++) {
+			int i = g.front().i;
+			int j = g.front().j;
+			int d = g.front().d;
+			int a = g.front().ans;
+			g.pop();
+			if (a==ss &&i == ansi && j == ansj && d == ansd) { cout << a; return; }
+			int k = 1;
+			if (a == ss) {
+				while (i + imap[d] * k > 0 && i + imap[d] * k <= m && j + jmap[d] * k > 0 && j + jmap[d] * k <= n && map[i + imap[d] * k][j + jmap[d] * k] == 0
+					&& k<=3) {
+					t.push({ i + imap[d] * k,j + jmap[d] * k,d,a + 1 });
+					k++;
+				}
+			}
+			else {
+				g.push({ i, j, d, a }); 
+			}
+		}
+		ss++;
 	}
 }
 
