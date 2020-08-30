12128f6a512dc7924fc767c049e62b887cc2a466
ohjiwooo <64720444+ohjiwooo@users.noreply.github.com>
2020-08-30 19:55:30 +0900

    commit

--- kakao/2020-6.cpp
+++ kakao/2020-6.cpp
@@ -9,141 +9,92 @@ using namespace std;
 bool com(int a,int b) {
 	return a > b;
 }
+int f_m(int a, int b) {
+
+	if (a > b) {
+		return a - b;
+	}
+	else {
+		return b - a;
+	}
+
+}
+
 int answer = 100;
 bool distarr[10] = { false, };
 bool weakarr[17] = { false, };
 
-void ff(vector<int> weak, vector<int> dist,int i,int n,int m,int ans,int now) {//출발 weak인덱스,,weak 원소갯수,간 사람수,남은 weak
+void ff(vector<int> weak, vector<int> dist, int i,int n,int m,int ans, int now) {//출발 weak인덱스,둘레 n,weak수 m,간사람수,남은 weak수
 
-	queue <int> qq;
-	if (ans >= dist.size() || now==-1) {
-		for (int i2 = 0; i2 < m; i2++) {
-			if (weak[i2] == false) {
-				return;
-			}
-		}
-
-		if (ans!=0 &&ans<answer) {
+	if (now==0) {
+		if (ans<answer) {
 			answer = ans;
 			return;
-
 		}
+		else { return; }
 	}
-	
-	for (int j = 0; j < dist.size(); j++) {//가는사람 조절- 가는사람 i
-		int f = 0; 
+	int k = (i+1)%m;
+	queue<int>qq;
+
+
+	for (int j = 0; j < dist.size();j++) {
 		if (distarr[j]==false) {
-			int k=(i+1)%m;
-			int a = 0;
-			int d = dist[j];
 			int t;
+			int d = dist[j];
+			distarr[j] = true; //j시작
+
+			weakarr[i] = true;
+			qq.push(i);
+			now--;
+
 			while (1) {
-				k = k % m;
-				if (weakarr[k] ==true) {
-					break;
-				}
-				if (k != 0) {
-					t = weak[k] - weak[k - 1];
+
+				if (k!=0) {
+					t = f_m(weak[k],weak[k-1]);
 				}
 				else {
-					t = (n - weak[m - 1]) + weak[k];
+					if(k==0) {
+						if (weak[k] < weak[m-1]) {
+							t = n - weak[m - 1] + weak[k];
+						}
+						else {
+							t = n - weak[k] + weak[m-1];
+						}
+					}
 				}
-				if (t <= d) {
-					weakarr[k] = true; d -= t; a++; f = 1; qq.push(k); k++; now--;
+
+				if (d>= t) {
+					d -= t; qq.push((k + 1) % m); weakarr[(k + 1) % m] = true; now--; k=(k+1)%m;
 				}
 				else { break; }
 			}
-			if (f==1) {
-				distarr[j] = true;
-				ff(weak, dist, k, n, m, ans + 1, now-1);
+
+			ff(weak, dist, k, n, m, ans + 1, now);
+			distarr[j] = false;
+			while (qq.empty()!=true) {
+				weakarr[qq.front()] = false;
+				qq.pop();
 				now++;
-				distarr[j] = false;
-				while (qq.empty()!=true) {
-					int tt = qq.front();
-					weakarr[tt] = false;
-					now++;
-					qq.pop();
-				}
 			}
 		}
 	}
 }
 
-
-void r_ff(vector<int> weak, vector<int> dist, int i, int n, int m, int ans, int now) {//출발 weak인덱스,,weak 원소갯수,간 사람수,남은 weak
-
-	queue <int> qq;
-	if (ans >= dist.size() || now == -1) {
-		for (int i2 = 0; i2 < m; i2++) {
-			if (weak[i2] == false) {
-				return;
-			}
-		}
-
-		if (ans != 0 && ans < answer) {
-			answer = ans;
-			return;
-
-		}
-	}
-	///////반대로가는거 고치기
-	for (int j = 0; j < dist.size(); j++) {//가는사람 조절- 가는사람 i
-		int f = 0;
-		if (distarr[j] == false) {
-			int k = (i + 1) % m;
-			int a = 0;
-			int d = dist[j];
-			int t;
-			while (1) {
-				k = k % m;
-				if (weakarr[k] == true) {
-					break;
-				}
-				if (k != 0) {
-					t = weak[k] - weak[k - 1];
-				}
-				else {
-					t = (n - weak[m - 1]) + weak[k];
-				}
-				if (t <= d) {
-					weakarr[k] = true; d -= t; a++; f = 1; qq.push(k); k++; now--;
-				}
-				else { break; }
-			}
-			if (f == 1) {
-				distarr[j] = true;
-				ff(weak, dist, k, n, m, ans + 1, now - 1);
-				now++;
-				distarr[j] = false;
-				while (qq.empty() != true) {
-					int tt = qq.front();
-					weakarr[tt] = false;
-					now++;
-					qq.pop();
-				}
-			}
-		}
-	}
-}
 
 int solution(int n, vector<int> weak, vector<int> dist) {
 
 	int m = weak.size();
 	sort(dist.begin(),dist.end(),com);
 	
-	for (int i = 0; i < weak.size();i++) {
-	//시작점 하나씩 조절
-		weakarr[i] = true;
-		ff( weak, dist, i, n,m,0,m-1);
+	for (int i = 0; i < weak.size();i++) {//시작인덱스 조절
+		ff( weak, dist, i,n,m,0,m); 
 		weakarr[i] = false;
 	}
 	reverse(weak.begin(), weak.end());
 	for (int i = 0; i < weak.size(); i++) {
-		//시작점 하나씩 조절
-		weakarr[i] = true;
-		ff(weak, dist, i, n, m, 0, m - 1);
+		ff(weak, dist, i, n, m,0,m);
 		weakarr[i] = false;
-		cout << answer << "\n";
+	
 	}
 
 	return answer;
@@ -154,9 +105,10 @@ int main() {
 
 	vector<int> weak;
 	vector<int> dist;
-
+	
 	weak.push_back(1);
 	weak.push_back(5);
+
 	weak.push_back(6);
 	weak.push_back(10);
 
